ab4d7a0a1f5a7fb2caaf15b860921593733198ea
akavick <akavick@tut.by>
2017-09-12 00:46:40 +0300

    1

--- TickTackToe3D/TickTackToe3D/MainWindow.xaml.cs
+++ TickTackToe3D/TickTackToe3D/MainWindow.xaml.cs
@@ -15,10 +15,7 @@ using System.Windows.Shapes;
 
 namespace TickTackToe3D
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
 
         public MainWindow()
@@ -26,5 +23,85 @@ namespace TickTackToe3D
             InitializeComponent();
 
         }
+
+
+        private void Method001()
+        {
+            /*
+                         <Button 
+                            xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+                            xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+                            Background="{x:Null}"
+                            Height="{x:Static SystemParameters.IconHeight}"
+                            Content="{Binding Path=Height, RelativeSource={RelativeSource Self}}"/>
+            */
+
+            /*
+
+                                 <Button 
+                                    xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+                                    xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml">
+                                     <Button.Background>
+                                        <x:Null/>
+                                     </Button.Background>
+                                     <Button.Height>
+                                        <x:Static Member="SystemParameters.IconHeight"/>
+                                     </Button.Height>
+                                     <Button.Content>
+                                         <Binding Path="Height">
+                                             <Binding.RelativeSource>
+                                                 <RelativeSource Mode="Self"/>
+                                             </Binding.RelativeSource>
+                                         </Binding>
+                                     </Button.Content>
+                                 </Button>
+
+            */
+
+
+            /* Content
+             
+             <Button xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation">
+                 <Rectangle Height ="40" Width="40" Fill="Black"/>
+             </Button >
+             
+             */
+
+
+            var b = new Button
+            {
+                Background = null,
+                Height = SystemParameters.IconHeight
+            };
+
+            var binding = new Binding
+            {
+                Path = new PropertyPath("Height"),
+                RelativeSource = RelativeSource.Self
+            };
+
+            b.SetBinding(ContentProperty, binding);
+        }
+
+
+
+        private void Method002()
+        {
+            /*
+             
+            <ListBox xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation">
+                <ListBox.Items>
+                    <ListBoxItem Content="Item 1"/>
+                    <ListBoxItem Content="Item 2"/>
+                </ListBox.Items>
+            </ListBox>
+             
+             */
+
+            var listbox = new ListBox();
+            listbox.Items.Add(new ListBoxItem { Content = "Item 1" });
+            listbox.Items.Add(new ListBoxItem { Content = "Item 2" });
+        }
+
     }
 }
