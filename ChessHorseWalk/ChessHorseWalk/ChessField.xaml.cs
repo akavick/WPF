c0e2c0b8363fc8440d29e1388930c246a4b9974c
akavick <akavick@PC-VL>
2017-09-26 18:36:10 +0300

    1

--- ChessHorseWalk/ChessHorseWalk/ChessField.xaml.cs
+++ ChessHorseWalk/ChessHorseWalk/ChessField.xaml.cs
@@ -1,56 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ChessHorseWalk
 {
     public partial class ChessField
     {
-        private const int Side = 8;
-        private Label _current = null;
-        private readonly Label[,] _cells = null;
+        private const int ClassicSideSize = 8;
+        private int _side;
+        private Label[,] _cells;
+        public int Side => _side;
+        public Label[,] Cells => _cells;
 
 
-        public ChessField()
+
+        public ChessField() : this(ClassicSideSize) { }
+
+        public ChessField(int side)
         {
             InitializeComponent();
-            _cells = new Label[Side, Side];
+            FillNewField(side);
+        }
+
+
+
+
+        public void FillNewField(int side = ClassicSideSize)
+        {
+            _field = new Grid();
+            _chessField.Content = _field;
+            _side = side;
+            _cells = new Label[_side, _side];
             FillField();
         }
 
 
+
         public void Clear()
         {
-            if (_current != null)
-            {
-                _current.Content = null;
-                _current.Foreground = Brushes.Black;
-                _current = null;
-            }
-            foreach (var cell in _cells)
-            {
-                cell.IsEnabled = true;
-                cell.Content = null;
-                cell.BorderBrush = Brushes.Black;
-                cell.Opacity = 1.0;
-            }
+            FillNewField(_side);
         }
+
 
 
         private void FillField()
         {
-            for (var i = 0;i < Side;i++)
+            for (var i = 0; i < _side; i++)
             {
                 _field.ColumnDefinitions.Add(new ColumnDefinition());
                 _field.RowDefinitions.Add(new RowDefinition());
@@ -59,36 +54,24 @@ namespace ChessHorseWalk
         }
 
 
+
         private void FillCells()
         {
-            for (var i = 0;i < Side;i++)
+            for (var i = 0; i < _side; i++)
             {
                 var isWhite = i % 2 == 0;
-                for (var j = 0;j < Side;j++)
+                for (var j = 0; j < _side; j++)
                 {
                     var cell = CreateLabel(isWhite);
                     _cells[i, j] = cell;
                     cell.SetValue(Grid.ColumnProperty, j);
                     cell.SetValue(Grid.RowProperty, i);
-                    var y = i;
-                    var x = j;
-
-                    void OnCellOnMouseUp(object s, MouseButtonEventArgs e)
-                    {
-                        cell.Content = (int?) cell.Content + 1 ?? 1;
-                        cell.Foreground = Brushes.Red;
-                        if (_current != null)
-                            _current.Foreground = Brushes.Black;
-                        _current = cell;
-                        //HandlePosition(y, x);
-                    }
-
-                    cell.MouseUp += OnCellOnMouseUp;
                     _field.Children.Add(cell);
                     isWhite = !isWhite;
                 }
             }
         }
+
 
 
         private Label CreateLabel(bool isWhite)
@@ -109,41 +92,6 @@ namespace ChessHorseWalk
             return cell;
         }
 
-
-        //private void HandlePosition(int i, int j)
-        //{
-        //    var possiblePositions = new[]
-        //    {
-        //        (y:i - 2, x:j - 1),
-        //        (y:i - 2, x:j + 1),
-        //        (y:i - 1, x:j + 2),
-        //        (y:i + 1, x:j + 2),
-        //        (y:i + 2, x:j + 1),
-        //        (y:i + 2, x:j - 1),
-        //        (y:i + 1, x:j - 2),
-        //        (y:i - 1, x:j - 2)
-        //    }
-        //    .Where(pp => pp.x > -1 && pp.x < Side && pp.y > -1 && pp.y < Side)
-        //    .ToArray();
-
-        //    foreach (var cell in _cells)
-        //    {
-        //        cell.IsEnabled = false;
-        //        cell.BorderBrush = Brushes.Black;
-        //        cell.Opacity = 0.8;
-        //    }
-
-        //    foreach (var pp in possiblePositions)
-        //    {
-        //        var cell = _cells[pp.y, pp.x];
-        //        cell.IsEnabled = true;
-        //        cell.BorderBrush = Brushes.LimeGreen;
-        //        cell.Opacity = 1.0;
-        //    }
-
-        //    _current.BorderBrush = Brushes.Red;
-        //    _current.Opacity = 1.0;
-        //}
 
     }
 }
--- ChessHorseWalk/ChessHorseWalk/MainWindow.xaml.cs
+++ ChessHorseWalk/ChessHorseWalk/MainWindow.xaml.cs
@@ -15,15 +15,178 @@ using System.Windows.Shapes;
 
 namespace ChessHorseWalk
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow
     {
+        private struct IntPoint
+        {
+            public int X;
+            public int Y;
+
+            public static bool operator ==(IntPoint left, IntPoint right) => left.X == right.X && left.Y == right.Y;
+            public static bool operator !=(IntPoint left, IntPoint right) => !(left == right);
+        }
+
+        private struct Cell
+        {
+            public IntPoint Place;
+            public int BestSteps;
+            public Label Label;
+
+            public void Reset()
+            {
+                Place.X = Place.Y = int.MinValue;
+                BestSteps = 0;
+                Label = null;
+            }
+        }
+
+        private Cell _startCell;
+        private Cell _finishCell;
+        private Cell[,] _cells;
+        private int _side;
+
         public MainWindow()
         {
             InitializeComponent();
-            _butReset.Click += (s, e) => _field.Clear();
+
+            _butReset.Click += (s, e) =>
+            {
+                _startCell.Reset();
+                _finishCell.Reset();
+                _field.Clear();
+                Reset();
+            };
+
+            _butStart.Click += async (s, e) => await RunProcess();
+
+            _butCancel.Click += (s, e) =>
+            {
+
+            };
+
+            Reset();
         }
+
+
+
+        private void Reset()
+        {
+            _side = _field.Side;
+            _butReset.IsEnabled = _butCancel.IsEnabled = _butStart.IsEnabled = false;
+            _cells = new Cell[_side, _side];
+
+            for (var y = 0; y < _side; y++)
+            {
+                for (var x = 0; x < _side; x++)
+                {
+                    _cells[y, x].Place.Y = y;
+                    _cells[y, x].Place.X = x;
+                    _cells[y, x].Label = _field.Cells[y, x];
+
+                    var c = _cells[y, x];
+                    _cells[y, x].Label.MouseUp += (s, e) =>
+                    {
+                        if (_startCell.Label == null)
+                        {
+                            _startCell = c;
+                            _startCell.Label.Content = "S";
+                        }
+                        else if (_finishCell.Label == null)
+                        {
+                            _finishCell = c;
+                            _finishCell.Label.Content = "F";
+                        }
+                    };
+                }
+            }
+
+            _butReset.IsEnabled = _butCancel.IsEnabled = _butStart.IsEnabled = true;
+        }
+
+
+
+
+
+
+        private async Task RunProcess()
+        {
+            await Dispatcher.InvokeAsync(() => _butReset.IsEnabled = _butStart.IsEnabled = false);
+            RunProcessRecursive(_startCell.Place, 0);
+            await Dispatcher.InvokeAsync(() => _butReset.IsEnabled = _butStart.IsEnabled = true);
+        }
+
+
+
+        private void RunProcessRecursive(IntPoint startFrom, int steps)
+        {
+            ++steps;
+
+            if()
+
+            if (startFrom == _finishCell.Place)
+            {
+                
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+        //void OnCellOnMouseUp(object s, MouseButtonEventArgs e)
+        //{
+        //    cell.Content = (int?)cell.Content + 1 ?? 1;
+        //    cell.Foreground = Brushes.Red;
+        //    if (_current != null)
+        //        _current.Foreground = Brushes.Black;
+        //    _current = cell;
+        //    //HandlePosition(y, x);
+        //}
+
+        //cell.MouseUp += OnCellOnMouseUp;
+
+
+
+
+        //private void HandlePosition(int i, int j)
+        //{
+        //    var possiblePositions = new[]
+        //    {
+        //        (y:i - 2, x:j - 1),
+        //        (y:i - 2, x:j + 1),
+        //        (y:i - 1, x:j + 2),
+        //        (y:i + 1, x:j + 2),
+        //        (y:i + 2, x:j + 1),
+        //        (y:i + 2, x:j - 1),
+        //        (y:i + 1, x:j - 2),
+        //        (y:i - 1, x:j - 2)
+        //    }
+        //    .Where(pp => pp.x > -1 && pp.x < Side && pp.y > -1 && pp.y < Side)
+        //    .ToArray();
+
+        //    foreach (var cell in _cells)
+        //    {
+        //        cell.IsEnabled = false;
+        //        cell.BorderBrush = Brushes.Black;
+        //        cell.Opacity = 0.8;
+        //    }
+
+        //    foreach (var pp in possiblePositions)
+        //    {
+        //        var cell = _cells[pp.y, pp.x];
+        //        cell.IsEnabled = true;
+        //        cell.BorderBrush = Brushes.LimeGreen;
+        //        cell.Opacity = 1.0;
+        //    }
+
+        //    _current.BorderBrush = Brushes.Red;
+        //    _current.Opacity = 1.0;
+        //}
     }
 }
